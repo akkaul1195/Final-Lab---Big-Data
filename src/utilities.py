28e1d7245951bf11590d961c01f5a317c2a1d398
23chrischen <23chrischen@gmail.com>
2016-05-01 16:36:47 -0500

    Starting on an svm classifier to ensemble results with logistic regression

---
+++ src/utilities.py
@@ -0,0 +1,36 @@
+from __future__ import division
+import os
+import numpy as np
+from sklearn import (metrics, cross_validation, linear_model, preprocessing)
+
+# Import this module like so:
+# from utilities import load_data, save_results
+
+def load_data(filename, use_labels=True):
+    """
+    Load data from CSV files and return them as numpy arrays
+    The use_labels parameter indicates whether one should
+    read the first column (containing class labels). If false,
+    return all 0s. 
+    """
+
+    # load column 1 to 8 (ignore last one)
+    data = np.loadtxt(open(filename), delimiter=',',
+                      usecols=range(1, 9), skiprows=1)
+    if use_labels:
+        labels = np.loadtxt(open(filename), delimiter=',',
+                            usecols=[0], skiprows=1)
+    else:
+        labels = np.zeros(data.shape[0])
+    return labels, data
+
+
+def save_results(predictions, filename):
+    """Given a vector of predictions, save results in CSV format."""
+
+    cwd = os.getcwd()
+    filepath = cwd + '/../output/' + filename
+    with open(filepath, 'w') as f:
+        f.write("id,ACTION\n")
+        for i, pred in enumerate(predictions):
+            f.write("%d,%f\n" % (i + 1, pred))
